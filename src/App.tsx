8bb91fe05d1cc18417817e5be55fc4ec6e8445bd
Aaron Cohen <acohen@mountainpasstech.com>
2020-01-30 14:16:20 +0200

    put components into separate divs

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,44 @@
 import { Helmet } from "react-helmet";
 import React from "react";
 
-import { Menu } from "./shared/menu";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import { About } from "./screens/about";
+import { Home } from "./screens/home";
 
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Helmet>
-          <meta charSet="utf-8" />
-          <link rel="canonical" href="https://smartlyrics.com" />
-        </Helmet>
-        <Menu />
-      </header>
-    </div>
+    <Router>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <link rel="canonical" href="https://smartlyrics.com" />
+      </Helmet>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <div id="page">
+          {/* A <Switch> looks through its children <Route>s and
+      renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 };
 

2f578dced71fc4db3a098e8dee669fc24e8c638e
Aaron Cohen <acohen@mountainpasstech.com>
2020-01-30 14:32:05 +0200

    make test pass

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import { render } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { queryAllByText } = render(<App />);
+  const el = queryAllByText(/Home/i)[0];
+  expect(el).toBeInTheDocument();
 });

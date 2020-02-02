86c70a4734213dc655c2cb95299d0d70b3a24921
Aaron Cohen <acohen@mountainpasstech.com>
2020-02-02 14:06:45 +0200

    configure store and add dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import { MainContent } from "./components/main-content";
 import { Head } from "./components/head";
 import { Header } from "./components/header";
+import { MainContent } from "./components/main-content";
+
+import configureStore from "./store";
+
+const store = configureStore({
+  fetchingSongsHasErrored: false,
+  songsAreLoading: false,
+  fetchingSongsHasSucceeded: false
+});
 
 const App = () => {
   return (
     <div className="container">
-      <Router>
-        <Head />
-        <div className="row">
-          <Header />
-        </div>
-        <div className="row">
-          <MainContent />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Head />
+          <div className="row">
+            <Header />
+          </div>
+          <div className="row">
+            <MainContent />
+          </div>
+        </Router>
+      </Provider>
     </div>
   );
 };
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,14 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
-
+import { compose, createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
 export default function configureStore(initialState: any) {
-    return createStore(
-        rootReducer,
-        initialState,
-        applyMiddleware(thunk)
-    );
+  const composeEnhancers =
+    // @ts-ignore
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(thunk))
+  );
 }

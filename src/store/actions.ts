788cce229f5b5c0ccc2757c058a58cca79167256
Aaron Cohen <acohen@mountainpasstech.com>
2020-02-02 08:58:33 +0200

    add  types and store

---
+++ src/store/actions.ts
@@ -0,0 +1,25 @@
+import { ChordProSong } from "../types";
+
+export const FETCHING_SONGS_HAS_ERRORED: string =  "FETCHING_SONGS_HAS_ERRORED";
+export const fetchingSongsHasErrored = (bool: boolean) => {
+  return {
+    type: FETCHING_SONGS_HAS_ERRORED,
+    hasErrored: bool
+  };
+};
+
+export const ITEMS_ARE_LOADING: string = "ITEMS_ARE_LOADING";
+export const fetchingSongsIsLoading = (bool: boolean) => {
+  return {
+    type: ITEMS_ARE_LOADING ,
+    isLoading: bool
+  };
+};
+
+export const FETCHING_SONGS_HAS_SUCCEEDED: string  = "FETCHING_SONGS_HAS_SUCCEEDED";
+export const fetchingSongsHasSucceeded = (items: Array<ChordProSong>) => {
+  return {
+    type: FETCHING_SONGS_HAS_SUCCEEDED,
+    items
+  };
+};

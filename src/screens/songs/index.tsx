2ab04eebc466b03b2b1479a37e096f66686f6f01
Aaron Cohen <acohen@mountainpasstech.com>
2020-02-09 15:24:49 +0200

    use table

--- src/screens/songs/index.tsx
+++ src/screens/songs/index.tsx
@@ -2,7 +2,7 @@ import { Helmet } from "react-helmet";
 import React, { useEffect } from "react";
 import { itemsFetchData } from "../../store/actions";
 import { useDispatch, useSelector } from "react-redux";
-import { ListGroup, ListGroupItem } from "reactstrap";
+import { Table } from "reactstrap";
 import { Song } from "../../types";
 
 const Songs = () => {
@@ -19,12 +19,25 @@ const Songs = () => {
         <title>Songs</title>
       </Helmet>
       <h2>Songs</h2>
-      <ListGroup>
-        {items.data &&
-          items.data.map((song: Song, i: number) => {
-            return <ListGroupItem key={i}>{song.title} {song.artist}</ListGroupItem>;
-          })}
-      </ListGroup>
+      <Table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Artist</th>
+          </tr>
+        </thead>
+        <tbody>
+          {items.data &&
+            items.data.map((song: Song, i: number) => {
+              return (
+                <tr key={i}>
+                  <td>{song.title}</td>
+                  <td>{song.artist}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </Table>
     </div>
   );
 };

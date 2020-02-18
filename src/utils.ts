a91117a99e84df9f446f0dd45e87338ad638f6f2
Aaron Cohen <acohen@mountainpasstech.com>
2020-02-18 23:24:40 +0200

    add wildfire

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,9 @@
 import ChordSheetJS from "chordsheetjs";
 
 export function parseChordProToHtml(chordPro: string) {
+  console.log('parsed')
+  const song = new ChordSheetJS.ChordProParser().parse(chordPro);
+  console.log(song);
   return new ChordSheetJS.HtmlTableFormatter().format(
     new ChordSheetJS.ChordProParser().parse(chordPro)
   );
